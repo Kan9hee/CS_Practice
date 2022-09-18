abf3dcf2a56a2f5f7a66853bbf6ee2d604dfc557
Kanghee Han <hcsy1219@gmail.com>
2022-09-19 00:34:02 +0900

    fix: BOJ Test1931

--- BOJ solve/Test1931.java
+++ BOJ solve/Test1931.java
@@ -1,47 +1,41 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Test1931 {
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static Map<Integer,Integer>map=new HashMap<>();
-    static Object keys[];
-    static int find(int num,int count){
-        count++;
-        System.out.println(count);
-        int key=(int)keys[num];
-        int val=map.get(key);
-        while(val>=key){
-            if(num+1!=keys.length)
-                key=(int)keys[num++];
-            else
-                break;
-        }
-        if(num+1<8)
-            find(key,count);
-        return count;
-    }
+	public static void main(String[] args) throws IOException{
+		int N = Integer.parseInt(br.readLine());
+		int timeTable[][] = new int[N][2];
 
-    public static void main(String[]args)throws IOException{
-        int test=Integer.parseInt(br.readLine());
-        int MAX=0;
-        for(int i=0;i<test;i++){
+		for (int i = 0; i < N; i++) {
             st=new StringTokenizer(br.readLine());
-            int pre_key=Integer.parseInt(st.nextToken());
-            int pre_val=Integer.parseInt(st.nextToken());
-            if(map.get(pre_key)!=null){
-                int low=Math.min(pre_val,map.get(pre_key));
-                map.replace(pre_key,low);
-            }else{
-                map.put(pre_key,pre_val);
-            }
+			timeTable[i][0] = Integer.parseInt(st.nextToken());
+			timeTable[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+        //두 회의의 종료시간을 기준으로 오름차순 정렬한다.
+        //단, 두 회의의 종료 시간이 같을 경우 시작 시간도 비교해 정렬한다.
+		Arrays.sort(timeTable, (a, b) -> {
+			if(a[1] == b[1])
+                return a[0] - b[0];
+			return a[1] - b[1];
+		});
+
+        for(int i=0;i<timeTable.length;i++){
+            System.out.println(timeTable[i][0]+" "+timeTable[i][1]);
         }
-        keys=map.keySet().toArray();
-        Arrays.sort(keys);
-        for(int i=0;i<keys.length;i++){
-            int temp=find(i,0);
-            MAX=Math.max(MAX, temp);
-        }
-        System.out.println(MAX);
-    }
+
+		int ans = 0;
+		int time = 0;
+
+		for (int i = 0; i < N; i++) {
+			if (timeTable[i][0] >= time) { 
+				time = timeTable[i][1];
+				ans++;
+			}
+		}
+		System.out.println(ans);
+        
+	}
 }
