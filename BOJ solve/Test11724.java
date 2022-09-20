fd733c3663e1dd59915cc294c6e83cbda78e9e2a
Kanghee Han <hcsy1219@gmail.com>
2022-09-21 00:16:27 +0900

    docs: Add explanation of Test11724

--- BOJ solve/Test11724.java
+++ BOJ solve/Test11724.java
@@ -1,15 +1,17 @@
 import java.io.*;
 import java.util.StringTokenizer;
-public class Test11724 {
+public class Test11724{
     static int N;
     static int M;
     static int nodes[][];
-    static boolean conected[];
+    static boolean connected[];
 
+    //그래프 내의 연결된 간선을 통하여
+    //connect배열의 (정점 번호)번째 요소를 true로 한다.
     static void dfs(int head){
-        conected[head]=true;
+        connected[head]=true;
         for(int i=1;i<=N;i++)
-            if(nodes[head][i]==1&&!conected[i])
+            if(nodes[head][i]==1&&!connected[i])
                 dfs(i);
     }
     public static void main(String[] args)throws IOException{
@@ -19,6 +21,9 @@ public class Test11724 {
         M=Integer.parseInt(st.nextToken());
         
         nodes=new int[N+1][N+1];
+
+        //선언한 두 정점은 서로 오갈 수 있는 간선으로 이어져
+        //이중 배열에서 두 값을 바꾼 경우도 1로 한다.
         for(int i=0;i<M;i++){
             st=new StringTokenizer(br.readLine());
             int head=Integer.parseInt(st.nextToken());
@@ -27,11 +32,16 @@ public class Test11724 {
             nodes[tail][head]=1;
         }
 
-        conected=new boolean[N+1];
+        connected=new boolean[N+1];
         int count=0;
 
+        //정점 1에서부터 N까지 세며
+        //i번째 connected배열에서 true였는지를 통해
+        //그 정점이 다른 정점과 이어졌는지를 분석한다.
+        //false일 경우, 지금까지 센 정점들과 이어지지 않았다고 간주한다.
+        //최소 1개의 그래프는 만들어진다.
         for(int i=1;i<=N;i++){
-            if(!conected[i]){
+            if(!connected[i]){
                 dfs(i);
                 count++;
             }
