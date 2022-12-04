ae029a1d5d9f8b22a393fcc881c3f3cc73145d2b
Kanghee Han <hcsy1219@gmail.com>
2022-12-04 22:46:47 +0900

    comment: add description about Test 14499

--- BOJ solve/Test14499.java
+++ BOJ solve/Test14499.java
@@ -17,41 +17,41 @@ public class Test14499 {
         if(tempX>=mapX||tempX<0||tempY>=mapY||tempY<0)
             return;
         
-        int val=dice[2];
-        switch(order) {
-            case 1:
+        int val=dice[2];    //주사위의 윗면
+        switch(order) {     //주사위의 이동 방향에 따라 회전한다. 여기서는 배열의 값을 옮기는 것으로 구현한다.
+            case 1:         //동쪽 이동
                 dice[2] = dice[3];
                 dice[3] = dice[5];
                 dice[5] = dice[1];
                 dice[1] = val;
                 break;
-            case 2:
+            case 2:         //서쪽 이동
                 dice[2] = dice[1];
                 dice[1] = dice[5];
                 dice[5] = dice[3];
                 dice[3] = val;
                 break;
-            case 3:
+            case 3:         //북쪽 이동
                 dice[2] = dice[4];
                 dice[4] = dice[5];
                 dice[5] = dice[0];
                 dice[0] = val;
                 break;
-            case 4:
+            case 4:         //남쪽 이동
                 dice[2] = dice[0];
                 dice[0] = dice[5];
                 dice[5] = dice[4];
                 dice[4] = val;
                 break;
         }
-        if(map[tempY][tempX] == 0) {
-            map[tempY][tempX] = dice[5];
-        } else {
-            dice[5] = map[tempY][tempX];
-            map[tempY][tempX] =0;
+        if(map[tempY][tempX] == 0) {    //주사위를 굴려 이동한 위치의 값이 0일 경우
+            map[tempY][tempX] = dice[5];//주사위의 밑면에 있는 값을 해당 위치에 복사한다.
+        } else {                        //그러나 이동해 도착한 위치의 값이 0이 아닐 경우
+            dice[5] = map[tempY][tempX];//해당 위치의 값을 주사위 밑면의 값으로 하고
+            map[tempY][tempX] =0;       //그 위치의 값은 0이 된다.
         }
-        sb.append(dice[2]).append("\n");
-        diceX=tempX;
+        System.out.println(dice[2]);    //현재 위치에서 주사위의 윗면에 있는 값을 출력한다.
+        diceX=tempX;    //이동한 위치를 갱신한다.
         diceY=tempY;
     }
 
@@ -78,7 +78,5 @@ public class Test14499 {
             int temp=Integer.parseInt(st.nextToken());
             moveDice(temp);
         }
-
-        System.out.println(sb.toString());
     }
 }
