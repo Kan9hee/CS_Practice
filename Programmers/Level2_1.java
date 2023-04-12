e5809cf4398e3a6049947f607b8044c108001a0d
han <hcsy1219@gmail.com>
2023-04-12 15:27:52 +0900

    Programmers Level2 solve1

---
+++ Programmers/Level2_1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Level2_1 {
+
+    public static void main(String args[]) {
+        System.out.println(solution("()()"));
+    }
+
+    private static boolean solution(String s) {
+        Queue<Character> check = new LinkedList<>();
+        boolean answer = true;
+
+        for (char part : s.toCharArray()) {
+            if (part == '(')
+                check.add(part);
+            else {
+                if (!check.isEmpty())
+                    check.remove();
+                else {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+
+        if (!check.isEmpty())
+            answer = false;
+
+        return answer;
+    }
+}
