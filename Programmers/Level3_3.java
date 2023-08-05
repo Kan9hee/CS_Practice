2e6bf8b4570e891a840a67f51142e164adf6b79a
han <hcsy1219@gmail.com>
2023-08-05 11:22:07 +0900

    Programmers Level3 solve3

---
+++ Programmers/Level3_3.java
@@ -0,0 +1,28 @@
+public class Level3_3 {
+    public static void main(String[] args) {
+        int[] numbers = { 9 };
+        System.out.println(solution(16, numbers, 2));
+    }
+
+    public static int solution(int n, int[] stations, int w) {
+        int answer = 0, place = 1;
+
+        for (int i = 0; i < stations.length; i++) {
+            if (place < stations[i] - w)
+                answer += deploy(place, stations[i] - w - 1, w);
+            place = stations[i] + w + 1;
+        }
+
+        if (stations[stations.length - 1] + w < n)
+            answer += deploy(stations[stations.length - 1] + w + 1, n, w);
+
+        return answer;
+    }
+
+    private static int deploy(int left, int right, int w) {
+        int result = (right - left + 1) / (2 * w + 1);
+        if ((right - left + 1) % (2 * w + 1) != 0)
+            result++;
+        return result;
+    }
+}
